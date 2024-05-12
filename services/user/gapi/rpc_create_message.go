8c2625de17af50292c29e5b7e765d4460b746583
vinayaknolastname <vinayakarya.pg@gmail.com>
2024-05-12 23:44:26 +0530

    s

--- services/user/gapi/rpc_create_message.go
+++ services/user/gapi/rpc_create_message.go
@@ -12,18 +12,18 @@ import (
 func (server *gAPI) SendMessage(ctx context.Context, req *user.CreateMessageRequest) (*user.CommonResponse, error) {
 
 	query := db.CreateMessageQuery()
-	result, err := server.Db.Db.Exec(query, req., req.GetPhoneNumber())
+	result, err := server.Db.Db.Exec(query)
 
 	if err != nil {
 		utils.LogSomething("err in creating pg", err.Error(), 0)
 	}
 
 	utils.LogSomething("user created", result, 0)
-	response := &user.UserResponse{ResData: &user.CommonResponse{
+	response := &user.CommonResponse{
 		StatusCode: http.StatusOK,
 		Success:    true,
 		Message:    "User Created",
-	}}
+	}
 
 	utils.LogSomething("user response", response, 1)
 
