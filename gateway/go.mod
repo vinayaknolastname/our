28fb38373f0c2e2502b2c30bbe9dc504f7fd00b4
vinayaknolastname <vinayakarya.pg@gmail.com>
2024-05-14 16:55:03 +0530

    fixed

--- gateway/go.mod
+++ gateway/go.mod
@@ -6,7 +6,8 @@ require (
 	github.com/golang/protobuf v1.5.4
 	github.com/lib/pq v1.10.9
 	github.com/sirupsen/logrus v1.9.3
-	github.com/vinayaknolastname/our v0.0.0-20240514050450-62abd6f44f93
+	github.com/vinayaknolastname/our v0.0.0-20240514110044-e7b0eae67100
+
 )
 
 require (
--- gateway/grpc/message_service.go
+++ gateway/grpc/message_service.go
@@ -17,7 +17,7 @@ func CreateReaction(service UserGrpcService, reactionData types.ReactionOnMessag
 	}
 
 	client := user.NewUserServiceClient(connection.conn)
-	resp, err := client.GetMessages(context.Background(), &user.GetMessageRequest{})
+	resp, err := client.ReactMessage(context.Background(), &user.SaveMessageReactionReq{MsgId: reactionData.MessageId, ReactorId: reactionData.ReactorId, ChatId: reactionData.ChatId, Reaction: reactionData.Reaction})
 	if err != nil {
 		log.Fatalf("User Created: %v", err)
 	}
@@ -26,7 +26,7 @@ func CreateReaction(service UserGrpcService, reactionData types.ReactionOnMessag
 	// respto := CommonResponse{
 	// 	statusCode: resp.ResData.StatusCode,
 	// 	success:    resp.ResData.Success,
-	// 	message:    resp.ResData.Message,
+	// 	message:    resp.ResData.Message,ccd
 	// }
 
 	utils.LogSomething("User Created  User", resp, 1)
