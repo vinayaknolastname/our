5f5240003dc3ebe145214eb7339096cc2c053d91
vinayaknolastname <vinayakarya.pg@gmail.com>
2024-05-25 10:56:16 +0530

    video_grpc

--- gateway/grpc/video_grpc.go
+++ gateway/grpc/video_grpc.go
@@ -1,11 +1,13 @@
 package grpcHandlers
 
 import (
-	"context"
-	"flag"
-	"io"
+	context "context"
+	"fmt"
 	"log"
-	"time"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/vinayaknolastname/our/gateway/utils"
@@ -14,10 +16,11 @@ import (
 )
 
 var (
-	// tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	// caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
-	serverAddrVideo = flag.String("addr", "localhost:9001", "The server address in the format of host:port")
-	// serverHostOverride = flag.String("server_host_override", "x.test.example.com", "The server name used to verify the hostname returned by the TLS handshake")
+// tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+// caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
+// serverAddrVideo = flag.String("addr", "localhost:9001", "The server address in the format of host:port")
+
+// serverHostOverride = flag.String("server_host_override", "x.test.example.com", "The server name used to verify the hostname returned by the TLS handshake")
 )
 
 type VideoGrpcService struct {
@@ -25,10 +28,10 @@ type VideoGrpcService struct {
 	videoServiceClient video.VideoServiceClient
 }
 
-var videoGrpcClient *VideoGrpcService
+var videoGrpcClient VideoGrpcService
 
 func ConnectWithVideoGrpcMiddleWare(c *gin.Context) {
-	utils.LogSomething("Calling ConnectVideoServiceGrpc --- Middle", connection.conn, 1)
+	utils.LogSomething("Calling ConnectVideoServiceGrpc --- Middle", videoGrpcClient.conn, 1)
 
 	ConnectWithVideoGrpc()
 	c.Next()
@@ -38,7 +41,7 @@ func ConnectWithVideoGrpc() {
 
 	if videoGrpcClient.conn == nil {
 
-		connection, err := grpc.NewClient(*serverAddrVideo, grpc.WithInsecure())
+		connection, err := grpc.NewClient("localhost:9001", grpc.WithInsecure())
 
 		if err != nil {
 			utils.LogSomething("err in dialing video grpc", err, 0)
@@ -46,7 +49,7 @@ func ConnectWithVideoGrpc() {
 
 		client := video.NewVideoServiceClient(connection)
 
-		videoGrpcClient = &VideoGrpcService{
+		videoGrpcClient = VideoGrpcService{
 			conn:               connection,
 			videoServiceClient: client,
 		}
@@ -54,12 +57,49 @@ func ConnectWithVideoGrpc() {
 }
 
 func StartVideoStream(c *gin.Context) {
+	inputVideo := "source_video.mp4"
+	// outputPrefix := "output_chunk"
 
-	videoData := &video.Video{
-		Content: []byte{111},
-	}
+	// dur, _ := getVideoDuration(inputVideo)
+
+	// var wg sync.WaitGroup
+
+	// numWorkers := 4
+	// chunks := make(chan int, numWorkers)
+
+	// for i := 0; i < numWorkers; i++ {
+	// 	wg.Add(1)
+	// 	go func() {
+	// 		defer wg.Done()
+	// 		for chunk := range chunks {
+	// 			log.Printf("Error splitting video chunk %d: %v", chunk)
+
+	// 			err := splitVideo(inputVideo, outputPrefix, chunk)
+	// 			if err != nil {
+	// 				log.Printf("Error splitting video chunk %d: %v", chunk, err)
+	// 			}
+	// 		}
+	// 	}()
+
+	// }
+
+	// for i := 0; i < dur; i++ {
+	// 	chunks <- i
+	// }
+	// close(chunks)
+
+	// // Wait for all worker goroutines to finish
+	// wg.Wait()
+	// splitVideo(inputVideo, outputPrefix, dur)
+
+	// ConvertBigVideoInSmallDurationChunks()
+	// videoData := &video.Video{
+	// 	Content: []byte{111},
+	// }
 
 	client := videoGrpcClient.videoServiceClient
+
+	utils.LogSomething("videoProces", videoGrpcClient.videoServiceClient, 0)
 
 	stream, err := client.VideoProccess(context.Background())
 
@@ -67,32 +107,97 @@ func StartVideoStream(c *gin.Context) {
 		utils.LogSomething("videoProces", err, 0)
 	}
 
-	waitc := make(chan struct{})
-	go func() {
-		for {
-			res, err := stream.Recv()
-			if err == io.EOF {
-				close(waitc)
-				return
-			}
-			if err != nil {
-				log.Fatalf("failed to receive a message : %v", err)
-			}
-			log.Printf("Received message from %s: %s", res.StatusCode, res.Message)
-		}
-	}()
+	buffer := make([]byte, 1024)
+	// go func() {
+	// 	for {
+	// 		// res, err := stream.Recv()
+	// 		// if err == io.EOF {
+	// 		// 	close(waitc)
+	// 		// 	return
+	// 		// }
+	// 		// if err != nil {
+	// 		// 	log.Fatalf("failed to receive a message : %v", err)
+	// 		// }
+	// 		// log.Printf("Received message from %s: %s", res.StatusCode, res.Message)
+	// 	}
+	// }()
 
 	// users := []string{"Alice", "Bob", "Charlie"}
+
+	file, err := os.Open(inputVideo)
+
+	if err != nil {
+		log.Fatalf("error reading file d: %v", err)
+	}
+	// defer file.Close()
+
 	go func() {
 		for {
-			if err := stream.Send(videoData); err != nil {
-				log.Fatalf("failed to send a message: %v", err)
+			bytesRead, err := file.Read(buffer)
+			if err != nil {
+				log.Println("error reading file ch: %v", err)
 			}
-			time.Sleep(1 * time.Second)
+
+			err = stream.Send(&video.Video{Content: buffer[:bytesRead]})
+			if err != nil {
+				log.Println("error sending chunk: %v", err)
+			}
+			if bytesRead == 0 {
+				file.Close()
+				break
+			}
 		}
-
 	}()
-	stream.CloseSend()
-	<-waitc
+	// go func() {
+	// 	for {
+	// 		if err := stream.Send(videoData); err != nil {
+	// 			log.Fatalf("failed to send a message: %v", err)
+	// 		}
+	// 		time.Sleep(1 * time.Second)
+	// 	}
 
+	// }()
+	// stream.CloseSend()
+
+}
+
+func splitVideo(inputVideo, outputPrefix string, duration int) error {
+	// Calculate the number of chunks
+	// numChunks := duration
+
+	// Split the video into chunks
+	// for i := 0; i < numChunks; i++ {
+	outputFile := fmt.Sprintf("%s_%d.mp4", outputPrefix, duration)
+	cmd := exec.Command("ffmpeg", "-ss", strconv.Itoa(duration), "-i", inputVideo, "-t", "1", "-c", "copy", outputFile)
+	if err := cmd.Run(); err != nil {
+		return err
+		// }
+	}
+	return nil
+}
+func ConvertBigVideoInSmallDurationChunks() {
+	inputVideo := "source_video.mp4"
+	outputPrefix := "output_chunk"
+
+	cmd := exec.Command("ffmpeg", "-i", inputVideo, "-c", "copy", "-segment_time", "1", "-f", "segment", outputPrefix+"%03d.mp4")
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Error splitting video: %v", err)
+	}
+
+	fmt.Println("Video split into 1-second chunks successfully.")
+}
+
+func getVideoDuration(inputVideo string) (int, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", inputVideo)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return 0, err
+	}
+	durationStr := strings.TrimSpace(string(output))
+	durationFloat, err := strconv.ParseFloat(durationStr, 64)
+	if err != nil {
+		return 0, err
+	}
+	duration := int(durationFloat)
+	return duration, nil
 }
--- services/video/gAPi/rpc_video_process.go
+++ services/video/gAPi/rpc_video_process.go
@@ -1,25 +1,216 @@
 package gApi
 
 import (
+	"fmt"
 	"io"
 	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
 
 	"github.com/vinayaknolastname/our/protobuf/video"
 )
 
 func (server *GrpcServer) VideoProccess(stream video.VideoService_VideoProccessServer) error {
+	inputVideo := "received_videos.mp4"
+	outputPrefix := "output_chunk"
+
+	file, err := os.Create(inputVideo)
+	if err != nil {
+		log.Printf("err in creating itemp img %s", err)
+
+		return err
+	}
+
 	for {
 		in, err := stream.Recv()
 		if err == io.EOF {
-			return nil
+			log.Printf("Received message from %s", err)
+
+			// return nil
 		}
+		if err != nil {
+			log.Printf("Received message from 12 %s", err)
+
+			// return err
+
+		}
+
+		// log.Printf("Received message from  22: %s", in.Content)
+
+		_, errf := file.Write(in.GetContent())
+
+		if err != nil {
+			log.Printf("Erro in writing file: %s", errf)
+			// return nil, err
+		}
+		log.Printf("Erro in writing file: %s", len(in.Content))
+
+		if len(in.Content) == 0 {
+			break
+		}
+
+		// if err := stream.Send(&video.CommonResponseVideo{StatusCode: 500, Message: "Hello ", Success: false}); err != nil {
+		// 	return err
+		// }
+	}
+
+	log.Printf("Erro in writing fildde: %s")
+
+	dur, err := getVideoDuration(inputVideo)
+
+	if err != nil {
+		log.Printf("Erro in writing file: %s", err)
+
+		return err
+	}
+	log.Printf("Erro in writing file: %s", dur)
+
+	numberOfWorker := 4
+
+	var wg sync.WaitGroup
+
+	chunks := make(chan int, numberOfWorker)
+
+	for i := 0; i < numberOfWorker; i++ {
+		log.Printf("l1: %s", i)
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for chunk := range chunks {
+				log.Printf("l1: %s", chunks)
+				splitVideo(inputVideo, outputPrefix, chunk)
+			}
+		}()
+	}
+
+	for i := 0; i < dur; i++ {
+		chunks <- i
+	}
+	println("weg")
+
+	wg.Wait()
+	println("wef in video resolution.")
+
+	changeVideoResolutionOperation()
+
+	return nil
+}
+
+func changeVideoResolutionOperation() {
+	println("err in video resolution.")
+	dir := "output_videos"
+	outputPrefix := "output_chunk"
+
+	numOfVideo, _ := countNumberOfVideos(dir)
+
+	numOfWorkers := 4
+
+	resolutions := []string{"640x360", "1280x720", "1920x1080"}
+
+	var wg sync.WaitGroup
+
+	videos := make(chan int, numOfWorkers)
+	for i := 0; i < numOfWorkers; i++ {
+		wg.Add(1)
+		defer wg.Done()
+		go func() {
+			for video := range videos {
+				file := fmt.Sprintf("%s_%d.mp4", outputPrefix, video)
+				input := filepath.Join(dir, file)
+				changeVideoResolution(input, resolutions[0], resolutions[0])
+			}
+		}()
+	}
+
+	for i := 0; i < numOfVideo; i++ {
+		videos <- i
+	}
+	wg.Wait()
+}
+
+func countNumberOfVideos(sourceDirectory string) (int, error) {
+	fileCount := 0
+
+	err := filepath.Walk(sourceDirectory, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		log.Printf("Received message from %s: %s", in.Content, in.Content)
-		if err := stream.Send(&video.CommonResponse{StatusCode: 500, Message: "Hello ", Success: false}); err != nil {
-			return err
+		if !info.IsDir() {
+			fileCount++
 		}
+		return nil
+	})
+
+	return fileCount, err
+}
+
+func changeVideoResolution(inputVideo string, resolution string, output string) {
+	// outputFilePath := filepath.Join(os.TempDir(), "converted_video_"+resolution+".mp4")
+	cmd := exec.Command("ffmpeg", "-i", inputVideo, "-vf", "scale="+resolution, output)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("err in video resolution.")
+
 	}
 
+}
+
+func splitVideo(inputVideo, outputPrefix string, duration int) error {
+	// Calculate the number of chunks
+	// numChunks := duration
+
+	outputDir := "output_videos"
+
+	err := os.MkdirAll(outputDir, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	// Split the video into chunks
+	// for i := 0; i < numChunks; i++ {
+	outputFile := fmt.Sprintf("%s_%d.mp4", outputPrefix, duration)
+	cmd := exec.Command("ffmpeg", "-ss", strconv.Itoa(duration), "-i", inputVideo, "-t", "1", "-c", "copy", filepath.Join(outputDir, outputFile))
+	if err := cmd.Run(); err != nil {
+		fmt.Println("Video split into 1-second chunks successfully.")
+
+		return err
+		// }
+	}
+	fmt.Println("Video split into 1-second chunks successfully.")
+
+	return nil
+}
+func ConvertBigVideoInSmallDurationChunks() {
+	inputVideo := "source_video.mp4"
+	outputPrefix := "output_chunk"
+
+	cmd := exec.Command("ffmpeg", "-i", inputVideo, "-c", "copy", "-segment_time", "1", "-f", "segment", outputPrefix+"%03d.mp4")
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Error splitting video: %v", err)
+	}
+
+	fmt.Println("Video split into 1-second chunks successfully.")
+}
+
+func getVideoDuration(inputVideo string) (int, error) {
+	cmd := exec.Command("ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", inputVideo)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return 0, err
+	}
+	durationStr := strings.TrimSpace(string(output))
+	durationFloat, err := strconv.ParseFloat(durationStr, 64)
+	if err != nil {
+		return 0, err
+	}
+	duration := int(durationFloat)
+	return duration, nil
 }
